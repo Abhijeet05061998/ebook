af24b3df015b55e6d1781505bff0738fa46530c4
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-02-16 00:13:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -102,7 +102,7 @@ particlesJS('particles-js',
           }
         },
         "bubble": {
-          "distance": 400,
+          "distance":450,
           "size": 40,
           "duration": 2,
           "opacity": 8,
