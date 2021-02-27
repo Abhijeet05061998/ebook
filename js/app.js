7cb8cd8903d22558f7e57b0ff39fb0a610ac83f6
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-02-27 22:23:37 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
