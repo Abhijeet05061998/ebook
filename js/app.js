1de2c8efb7b46a14cb97c064102944390140b296
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-02-27 22:23:50 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
+
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
